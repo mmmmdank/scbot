f22abf03dc92c38a00c2c102a1ac1de10056e44e
mmmmdank <khripunov@yahoo.com>
2014-05-23 21:56:45 +0000

    wefs

--- sc-inc.php
+++ sc-inc.php
@@ -89,8 +89,8 @@ class sc {
         foreach ($xml->children() as $xmlgroup) {
             $group = array();
             foreach($xmlgroup->children() as $attr) {
-                if($attr->getName() == "id") {$group['id']=$attr;}
-                if($attr->getName() == "name") {$group['name']=$attr;}
+                if($attr->getName() == "id") {$group['id']=$attr[0];}
+                if($attr->getName() == "name") {$group['name']=$attr[0];}
             }
             if($group['id'] && $group['name']) {
                 array_push($groups,$group);
