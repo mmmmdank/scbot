74946253b25e1b073b43170f14689aba64dee320
Ubuntu <ubuntu@ip-172-31-40-151.us-west-2.compute.internal>
2014-05-25 02:34:05 +0000

    ass

--- db.php
+++ db.php
@@ -33,8 +33,11 @@ class scbotdb {
     }
     
     public function doStatsAlreadyExistsForToday(){
-        $r = mysql_query("SELECT * FROM 'json_stats_dump' where DATE='".date("Ymd")."'", $this->link) || die(mysql_error());
-        $row = mysql_fetch_array($r, MYSQL_ASSOC);
+        $r = mysql_query("SELECT * FROM json_stats_dump where DATE='".date("Ymd")."'", $this->link);
+if (!$r) {
+    die('Invalid query: ' . mysql_error());
+}        
+$row = mysql_fetch_array($r, MYSQL_ASSOC);
         return $row && count($row)>0;
     }
     
