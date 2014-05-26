d846699e1356fcf37660ed08677973ab3d7e4ceb
mmmmdank <khripunov@yahoo.com>
2014-05-26 20:43:38 +0000

    qweee

--- index.php
+++ index.php
@@ -43,10 +43,6 @@ else {
             ?>
         </p>
         Counters:<br>
-        daily shares: <?php if (isset($counters['SHARE_COUNTER']) {print($counters['SHARE_COUNTER']);} ;?><br>
-        daily unshares: <?php if (isset($counters['UNSHARE_COUNTER']) {print($counters['UNSHARE_COUNTER']);} ;?><br>
-        group polls: <?php if (isset($counters['GROUP_POLL_COUNTER']) {print($counters['GROUP_POLL_COUNTER']);} ;?><br>
-
 <?php
 }
 ?>
