ff744f63ac08d101d33eac73b1d004bae71cd0bf
mmmmdank <khripunov@yahoo.com>
2014-05-27 02:22:31 +0000

    qweee

--- db.php
+++ db.php
@@ -67,8 +67,8 @@ class scbotdb {
         if($current_counters!=false) {
             print('<h1>1</h1>');
             $daily_counter = (int)$current_counters[$which];              
-            $r = mysql_query("update daily_share_counter set ".$which."='". $daily_counter++ ."' where DATE='".date("Ymd")."'", $this->link);
-            print("<h4>".$daily_counter."</h4><h4>"."update daily_share_counter set ".$which."='". $daily_counter++ ."' where DATE='".date("Ymd")."'"."</h4>");
+            $r = mysql_query("update daily_share_counter set ".$which."='". ((int)$daily_counter)++ ."' where DATE='".date("Ymd")."'", $this->link);
+            print("<h4>".$daily_counter."</h4><h4>"."update daily_share_counter set ".$which."='". ((int)$daily_counter)++ ."' where DATE='".date("Ymd")."'"."</h4>");
             var_dump($r);
             return $r;            
         }
