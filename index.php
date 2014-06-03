072951b13761f1dd1b4952cfb745bf061280b57c
mmmmdank <khripunov@yahoo.com>
2014-06-03 22:50:35 +0000

    as

--- index.php
+++ index.php
@@ -52,8 +52,6 @@ else {
     </head>
 
     <body>
-        <h1>s</h1>
-        ThingS: <br>
         All songs are currently shared to: <?php print($sc_stats['groupStats']['groups_total']); ?> groups;<br>
         Like this:<br>
         <p style="padding:20px;">
