931d7caf815bec6fb75f2d3756479665b5a55d40
mmmmdank <khripunov@yahoo.com>
2014-06-17 19:25:22 +0000

    d

--- config.php
+++ config.php
@@ -5,15 +5,16 @@ class scbotconfig {
     public $dbUser="root";
     public $dbPass="bujurocks";
     
-    public $tracks = array( "cow"=>"106944823"
+    public $tracks = array( "hum"=>"138695288"
+        					,"cow"=>"106944823"
                             ,"whales"=>"103486177"
                             ,"goldilox"=>"99583408"
                             ,"bears"=>"97204021"
 			                ,"forklift"=>"95783468"
 			                ,"firetrucks"=>"91741317"
                            /* ,"chaka"=>"79674516"
-                            ,"capleton"=>"77591808"*/
-                            ,"tractors"=>"87757601"
+                            ,"capleton"=>"77591808"
+                            ,"tractors"=>"87757601"*/
                             /*,"fela"=>"72433836"*/
                             ,"magnumpig"=>"68747384");    
 
